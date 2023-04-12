a6720b5a767f63ab2157d66b920ab64642aa4136
Jeanne Colmou <jeannecolmou@macbook-pro-de-jeanne.home>
2023-04-12 10:43:39 +0200

    have to fill everything, version of Shima and I

---
+++ inc/Tree.hpp
@@ -0,0 +1,67 @@
+#ifndef _TREE_HPP
+#define _TREE_HPP
+
+#include "ft_irc.hpp"
+
+class Tree
+{
+    private:
+        std::map<std::string, User*>      _user_to_nick;
+        std::map<int, User*>              _user_to_fd;
+        std::map<std::string, Channel>    _chto_channel;//nicknames of all channels opened
+
+    public:
+        Tree() {};
+        ~Tree() {};
+
+        std::map<std::string, Channel>  get_channel(){return (_chto_channel);}
+        std::map<std::string, User*>    get_usernick(){return (_user_to_nick);}
+        std::map<int, User*>            get_userfd(){return (_user_to_fd);}
+
+        void    insert(const std::string &nick_name, int    &fd)
+        {
+            User    *user = new User(nick_name, fd);
+            _user_to_nick.insert(make_pair(nick_name, user));
+            _user_to_fd.insert(std::make_pair(fd, user));
+        }
+
+        void    insert(const std::string &chan_name)
+        {
+            Channel chan();
+            _chto_channel.insert(std::make_pair(chan_name, chan));
+        }
+
+        User    *find_usr_by_nickname(std::string& nickname)
+        {
+            return((_user_to_nick.find(nickname))->second);
+        }
+
+        User    *find_usr_by_fd(int& fd)
+        {
+            return(_user_to_fd.find(fd)->second);
+        }
+
+        Channel   &find_channel(std::string& channel)
+        {
+            return((_chto_channel.find(channel))->second);
+        }
+
+        void    erase_user(User &user)
+        {
+            _user_to_nick.erase(user._nickname);
+            _user_to_fd.erase(user._fd);
+            user.erase_me_from_allchannel();
+
+            return ;
+        }
+
+        void    erase_channel(std::string   _name)
+        {
+            _chto_channel.erase(_name);
+        }
+
+
+        //modify nickname
+};
+
+#endif
---
+++ inc/ft_irc.hpp
@@ -0,0 +1,32 @@
+#ifndef _FT_IRC_HPP
+#define _FT_IRC_HPP
+
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <iostream> // For cout
+#include <unistd.h> // For read
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <sys/time.h>
+#include <cstring>
+#include <stdexcept>
+
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+
+#include "Parser.hpp"
+#include "User.hpp"
+#include "Server.hpp"
+#include "Tree.hpp"
+#include "Channel.hpp"
+
+
+#define TIMEOUT 3
+#define BUFFER_SIZE 1024
+#endif
